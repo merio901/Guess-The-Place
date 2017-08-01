33733c24c6f69c76779fc506d5259a304894cc76
Michal Sobczak <micsob901@wp.pl>
2017-08-01 12:33:41 +0200

    starting repo files

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, './src/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+   rules: [
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       use: {
+         loader: 'babel-loader',
+         options: {
+           presets: ['env']
+         }
+       }
+     },
+    {
+      test: /\.css$/,
+      use: [ 'style-loader', 'css-loader', "postcss-loader" ]
+    },
+      {
+         test: /\.(woff|woff2)$/,
+         loader: 'file-loader?name=fonts/[name].[ext]'
+     },
+    {
+     test: /\.scss$/,
+     use: [ 'style-loader', 'css-loader', "postcss-loader", 'sass-loader' ]
+   },
+    {
+       test: /\.(png|jpe?g|gif|svg|ico)$/,
+       use: 'file-loader?name=images/[name].[ext]'
+     },
+   ]
+ },
+ plugins: [
+   require('autoprefixer'),
+   new webpack.SourceMapDevToolPlugin({
+  filename: 'bundle.js.map',
+  exclude: ['/node_modules/','vendor.js']
+})
+ ]
+};
