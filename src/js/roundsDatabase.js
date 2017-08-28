b4a81dc08d67c625310fe44b71fe699266cfbf85
merio901 <micsob901@wp.pl>
2017-08-28 15:25:12 +0200

    added commas to database

--- src/js/roundsDatabase.js
+++ src/js/roundsDatabase.js
@@ -1,78 +1,78 @@
 export const roundsDatabase = [
   {
-    location: "Prosta Warsaw Poland"
+    location: "Prosta, Warsaw, Poland"
   },
   {
-    location: "Vestervang Aarhus Denmark"
+    location: "Vestervang, Aarhus, Denmark"
   },
   {
-    location: "OldFordRd London England"
+    location: "Old Ford Rd, London, England"
   },
   {
-    location: "Wall St New York USA"
+    location: "Broadway, New York, USA"
   },
   {
-    location: "Świętokrzyska Warsaw Poland"
+    location: "Świętokrzyska, Warsaw, Poland"
   },
   {
-    location: "California St San Francisco USA"
+    location: "California St, San Francisco, USA"
   },
   {
-    location: "Toulouse Street New Orleans USA"
+    location: "Toulouse Street, New Orleans, USA"
   },
   {
-    location: "Adderley Cape_Town RPA"
+    location: "Adderley, Cape Town, RPA"
   },
   {
-    location: "Gunnersbury Avenue London United Kingdom"
+    location: "Gunnersbury Avenue, London, United Kingdom"
   },
   {
-    location: "Chłodna Warsaw Poland"
+    location: "Chłodna, Warsaw, Poland"
   },
   {
-    location: "Aleja KEN Warsaw Poland"
+    location: "Komisji Edukacji Narodowej, Warszawa, Poland"
   },
   {
-    location: "The Mall Londyn England"
+    location: "The Mall, Londyn, England"
   },
   {
-    location: "Beykoz Polonezkoy Turcja"
+    location: "Kennedy Cd, Stambuł, Turcja"
   },
   {
-    location: "Dairyman Close London United Kingdom"
+    location: "Dairyman Close, London, United Kingdom"
   },
   {
-    location: "Miedziana Warsaw Poland"
+    location: "Miedziana, Warsaw, Poland"
   },
   {
-    location: "Baker Street London United Kingdom"
+    location: "Baker Street, London, United Kingdom"
   },
   {
-    location: "Spadina Ave Toronto Ontario Canada"
+    location: "Spadina Ave, Toronto Ontario, Canada"
   },
   {
-    location: "The Quay Ilfracombe United Kingdom"
+    location: "The Quay, Ilfracombe, United Kingdom"
   },
   {
-    location: "Bulgaria Mexico_City Mexico"
+    location: "Bulgaria, Mexico City, Mexico"
   },
   {
-    location: "Carrer de Mallorca Barcelona Spain"
+    location: "Carrer de Mallorca, Barcelona, Spain"
   },
   {
-    location: "5th Avenue New York City USA"
+    location: "5th Avenue, New York, City USA"
   },
   {
-    location: "Via Celio Vibenna Rome Italy"
+    location: "Via Celio Vibenna, Rome, Italy"
   },
   {
-    location: "The Square Marazion United Kingdom"
+    location: "The Square, Marazion, United Kingdom"
   },
   {
-    location: "Pulsera Algeciras Spain"
+    location: "Pulsera, Algeciras, Spain"
   },
   {
-    location: "Avinguda de l'Autopista del Saler Valencia Spain"
+    location: "Avinguda de l'Autopista del Saler, Valencia, Spain"
   },
 
 ]
